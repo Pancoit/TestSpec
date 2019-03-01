cf69e633a7016429ceaad89e6e908aa5c7b7b35b
HXRoot <907029382@qq.com>
2019-03-01 13:57:56 +0800

    init

---
+++ TestSpec/Source/Source.swift
@@ -0,0 +1,15 @@
+//
+//  Source.swift
+//  TestSpec
+//
+//  Created by iMac on 2019/3/1.
+//  Copyright © 2019 广州磐钴智能科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class Source: NSObject {
+    func getSource() -> String {
+        return "a source"
+    }
+}
---
+++ TestSpec/TestSpec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSpec
+//
+//  Created by iMac on 2019/3/1.
+//  Copyright © 2019 广州磐钴智能科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
